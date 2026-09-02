fa7a4174225c5f9823cd17c4b240699eaaee9188
LinhNguyenNgoc <75782178+LinhNguyenNgoc@users.noreply.github.com>
2026-09-02 14:41:30 +0700

    feat: implement inter-service communication between order-service and inventory-service using OpenFeign

--- order-service/src/main/java/com/example/order/service/OrderService.java
+++ order-service/src/main/java/com/example/order/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.example.order.client.InventoryClient;
 import com.example.order.dto.OrderRequest;
 import com.example.order.model.Order;
 import com.example.order.repository.OrderRepository;
@@ -15,14 +16,21 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+    	var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 
 }
