b85f280181b51e7fc633ba88a5612a3349fb0a2c
LinhNguyenNgoc <75782178+LinhNguyenNgoc@users.noreply.github.com>
2026-09-03 21:03:38 +0700

    feat: implement circuit breaker for inventory client

--- order-service/src/main/java/com/example/order/client/InventoryClient.java
+++ order-service/src/main/java/com/example/order/client/InventoryClient.java
@@ -1,7 +1,14 @@
 package com.example.order.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+//import groovy.util.logging.Slf4j;
+
 
 //@FeignClient(value = "inventory", url = "${inventory.url}")
 //public interface InventoryClient {
@@ -10,8 +17,24 @@ import org.springframework.web.service.annotation.GetExchange;
 //    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 //}
 
+//public interface InventoryClient {
+//
+//    @GetExchange("/api/inventory")
+//    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+//}
+
+//@Slf4j
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skucode {}, failure reason: {}", code, throwable.getMessage());
+        return false;
+    }
 }
